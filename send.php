f69b9cb57bbb3fb00d5cdc5403df42dee3194401
Destrucciones <73138509+Destrucciones@users.noreply.github.com>
2020-10-29 11:23:26 -0600

    Update and rename enviar.php to send.php

--- send.php
+++ send.php
@@ -13,7 +13,7 @@ $mensaje .= "Su e-mail es: " . $mail . " \r\n";
 $mensaje .= "Mensaje: " . $_POST['mensaje'] . " \r\n";
 $mensaje .= "Enviado el " . date('d/m/Y', time());
 
-$para = 'ventasdestruccionesdem@gmail.com';
+$para = 'nekone1276@gmail.com';
 $asunto = 'Contacto desde Taller Webmaster';
 
 mail($para, $asunto, utf8_decode($mensaje), $header);
